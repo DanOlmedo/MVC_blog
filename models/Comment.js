037630c720842b77f74c51454629759fe324e6a6
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-07-07 19:04:47 -0500

    Added comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,33 @@
 // Comment model
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        
+          commentText: {
+              type: DataTypes.STRING,
+              allowNull: false,
+          },
+
+    }, 
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+        
+    }
+)
+
+module.exports = Comment;
